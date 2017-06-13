ffc5d9270b8439e372023093b6e0e368f5a0124a
Manuel Zaforas <mzaforas@paradigmadigital.com>
2017-06-14 00:02:59 +0200

    working on adapt producer script

--- kinesis/producer.sh
+++ kinesis/producer.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
-while true
-do
-  eventTime=$(date +"%Y-%m-%d-%T")
-  userId=$(( ( RANDOM % 100000 )  + 1 ))
-  appId=$(( ( RANDOM % 1000 )  + 1 ))
-  appScore=$(( ( RANDOM % 100 )  + 1 ))
-  appData=SomeTestData
-  echo "$eventTime,$userId,$appId,$appScore,$appData"
-  aws kinesis put-record --stream-name iniciativa-big-data --data "$eventTime,$userId,$appId,$appScore,$appData"$'\n' --partition-key $appId --region es-west-1
-done
+
+v=0.3
+
+echo $v
+#
+#while true
+#do
+#    VendorID=$(( ( RANDOM % 2) + 1 ))
+#    tpep_pickup_datetime=$(date +"%Y-%m-%d %T")
+#    tpep_dropoff_datetime=$(date +"%Y-%m-%d %T" -d "+$(( RANDOM % 60 ))minutes")
+#    passenger_count=$(( ( RANDOM % 9) + 1 ))
+#    trip_distance=$(( ( RANDOM % 10) + 1 ))
+#    pickup_longitude=!!
+#    pickup_latitude=!!
+#    RatecodeID=$(( ( RANDOM  % 6) + 1 ))
+#    store_and_fwd_flag=!!
+#    dropoff_longitude=!!
+#    dropoff_latitude=!!
+#    payment_type=$(( ( RANDOM  % 5) + 1 ))
+#    fare_amount=!!
+#    extra=$(( ( RANDOM  % 3) ))
+#    mta_tax=!!
+#    tip_amount=!!
+#    tolls_amount=!!
+#    improvement_surcharge=0.3
+#    total_amount=sumatorio
+#
+#  eventTime=$(date +"%Y-%m-%d-%T")
+#  userId=$(( ( RANDOM % 100000 )  + 1 ))
+#  appId=$(( ( RANDOM % 1000 )  + 1 ))
+#  appScore=$(( ( RANDOM % 100 )  + 1 ))
+#  appData=SomeTestData
+#  echo "$eventTime,$userId,$appId,$appScore,$appData"
+#  aws kinesis put-record --stream-name iniciativa-big-data --data "$eventTime,$userId,$appId,$appScore,$appData"$'\n' --partition-key $appId --region eu-west-1
+#done
